33981e658e2169d4302ceee7a43fa3a87be0d8be
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-07-31 18:17:46 +0100

    Added Form Validation

--- src/components/FormPersonalDetails.js
+++ src/components/FormPersonalDetails.js
@@ -1,17 +1,48 @@
 import React, { Component } from "react";
 
 class FormPersonalDetails extends Component {
-  continue = (e) => {
-    // PROCESS THE FORM WHEN YOU LEARN BACKEND
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       formErrors: {},
+       isSubmit: false
+    }
+  }
 
-    e.preventDefault();
-    this.props.nextStep();
-  };
+  componentDidMount() {
+    console.log(this.state.formErrors)
+    if (Object.keys(this.state.formErrors).length === 0 && this.state.isSubmit) {
+      console.log(this.props.values)
+    }
+  }
 
   back = (e) => {
     e.preventDefault();
     this.props.prevStep();
+  }
+
+  continue = (e) => {
+    e.preventDefault();
+    this.setState({isSubmit: true})
+    this.setState({formErrors:this.validate(this.props.values)})
+    if (Object.keys(this.state.formErrors).length === 0 && this.state.isSubmit) {
+      this.props.nextStep()
+    }
   };
+
+  validate = (values) => {
+    const errors = {}
+
+    if (!values.city) {
+      errors.city = "City is required!"
+    }
+    if (!values.age) {
+      errors.age = "Age is required!"
+    }
+    return errors
+  }
+  
 
   render() {
     const { values, handleChange } = this.props;
@@ -28,6 +59,7 @@ class FormPersonalDetails extends Component {
             onChange={handleChange("city")}
             placeholder="City"
           />
+          <p>{ this.state.formErrors.city }</p>
           <input
             type="text"
             value={values.occupation}
@@ -40,6 +72,7 @@ class FormPersonalDetails extends Component {
             onChange={handleChange("age")}
             placeholder="Age"
           />
+          <p>{ this.state.formErrors.age }</p>
           <input
             type="text"
             value={values.bio}
--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -2,15 +2,53 @@ import React, { Component } from "react";
 import '../App.css'
 
 class FormUserDetails extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       formErrors: {},
+       isSubmit: false
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.state.formErrors)
+    if (Object.keys(this.state.formErrors).length === 0 && this.state.isSubmit) {
+      console.log(this.props.values)
+    }
+  }
+
+
   continue = (e) => {
     e.preventDefault();
-    this.props.nextStep();
+    this.setState({isSubmit: true})
+    this.setState({formErrors:this.validate(this.props.values)})
+    if (Object.keys(this.state.formErrors).length === 0 && this.state.isSubmit) {
+      this.props.nextStep()
+    }
   };
+
+  validate = (values) => {
+    const errors = {}
+    const regex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (!values.firstName) {
+      errors.firstName = "First Name is required!"
+    }
+    if (!values.lastName) {
+      errors.lastName = "Last Name is required!"
+    }
+    if (!values.email) {
+      errors.email = "Email is required!"
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email!"
+    }
+    return errors
+  }
 
   render() {
     const { values, handleChange } = this.props;
     
-
     return (
       <>
         <header><h1>Login Details</h1></header>
@@ -21,18 +59,21 @@ class FormUserDetails extends Component {
             onChange={ handleChange('firstName') }
             placeholder="First Name"
           />
+          <p>{ this.state.formErrors.firstName }</p>
           <input
             type="text"
             value={ values.lastName }
             onChange={ handleChange('lastName') }
             placeholder="Last Name"
           />
+          <p>{ this.state.formErrors.lastName }</p>
           <input
-            type="text"
+            type="email"
             value={ values.email }
             onChange={ handleChange('email') }
             placeholder="Email"
           />
+          <p>{ this.state.formErrors.email }</p>
           <button onClick={ this.continue }>Continue</button>
         </form>
       </>
