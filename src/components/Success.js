974d2aae08665a44d2c2ba4f900bfe7222cdfb10
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-08-10 14:59:44 +0100

    Optimisation

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+function handleSubmit()  {
+  alert('Learn Routers')
+}
+
 function Success() {
   return (
     <>
-        <header><h1>Suceess</h1></header>
+        <header><h1>Success</h1></header>
         <h1>You have completed the form</h1>
         <h4>Further Information will be passed</h4>
+        <button onClick={handleSubmit}>Continue</button>
     </>
   )
 }
